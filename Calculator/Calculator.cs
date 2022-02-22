dcfd88994526ee2afdd52d0ca0f474b4e42930d1
Binbo55 <hieuduke123@gmail.com>
2022-02-22 16:57:00 +0700

    project last

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -1,26 +1,15 @@
-namespace UtilityKnife.Calculator
+using System;
+using UtilityKnife.Converters;
+namespace UtilityKnife
 {
- public static class Calculator
- {
- public static double Add(double x, double y)
- {
- return x + y;
- }
- public static double Subtract(double x, double y)
- {
- return x - y;
- }
- public static double Multiply(double x, double y)
- {
- return x * y;
- }
- public static int Division (int x, int y)
- {
- return x / y;
- }
- public static double Division (double x, double y)
- {
- return x / y;
- }
- }
+
+ public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var converter = new FahrenheitToCelsius();
+            var celsius = converter.FahrenheitToCelsiusConverter(212.0);
+            Console.WriteLine($"Fahrenheit temp of 212 is {celsius}.");
+        }
+    }
 }
